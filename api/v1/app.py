3f5921504cf3fa37ac8a882bc64ee8c5d0683f4c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-05 08:27:44 +0200

    use port as an int

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,9 @@ script to create a variable app, instance of Flask
 to start our web app
 """
 
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
@@ -22,5 +22,5 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     hbnb_api_host = getenv('HBNB_API_H', '0.0.0.0')
-    hbnb_api_port = getenv('HBNB_API_PORT', '5000')
+    hbnb_api_port = getenv('HBNB_API_PORT', 5000)
     app.run(host=hbnb_api_host, port=hbnb_api_port, threaded=True)
