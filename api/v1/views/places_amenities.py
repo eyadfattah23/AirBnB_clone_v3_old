dcbaab444a5557aaad33dbce8d9d419b867cf454
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-06 04:59:43 +0200

    delete

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """view for the link between Place objects and Amenity objects"""
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import abort, jsonify
 from models import storage
 from models.place import Place
 from models.amenity import Amenity
+from models import storage_t
 
 
 @app_views.route("places/<place_id>/amenities", strict_slashes=False)
@@ -24,17 +25,26 @@ def delete_place_amenity(place_id, amenity_id):
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
-    is_amenity_in_place = False
-
-    for amen in place.amenities:
-        if amenity_id == amen.id:
-            is_amenity_in_place = True
-
-    if not place or not amenity or not is_amenity_in_place:
+    if not place or not amenity:
         abort(404)
-    storage.delete(amenity)
+
+    if storage_t == 'db':
+        amenity_in_place = False
+        for amen in place.amenities:
+            if amen.id == amenity.id:
+                amenity_in_place = True
+                break
+        if not amenity_in_place:
+            abort(404)
+        storage.delete(amenity)
+    else:
+        try:
+            place.amenity_ids.remove(amenity_id)
+        except Exception as exception:
+            abort(404)
+
     storage.save()
-    return jsonify({}), 200
+    return jsonify({})
 
 
 @app_views.route("places/<place_id>/amenities/<amenity_id>", methods=['POST'])
@@ -43,10 +53,12 @@ def place_amenity(place_id, amenity_id):
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
-    for amen in place.amenities:
-        if amenity_id == amen.id:
-            return jsonify(amenity.to_dict()), 200
+    if not place or not amenity:
+        abort(404)
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
 
-    place.amenities.append(amenity.to_dict())
+    amenity.place_id = place_id
+    place.amenities.append(amenity)
     storage.save()
     return jsonify(amenity.to_dict()), 201
